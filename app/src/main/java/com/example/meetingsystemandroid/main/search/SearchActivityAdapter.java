6e291ed51c0c279ae3c60cd20215fa91364b019d
y4ngyy <591620892@qq.com>
2019-08-28 17:28:43 +0800

    增加搜索样式

--- app/src/main/java/com/example/meetingsystemandroid/main/search/SearchActivityAdapter.java
+++ app/src/main/java/com/example/meetingsystemandroid/main/search/SearchActivityAdapter.java
@@ -1,22 +1,37 @@
 package com.example.meetingsystemandroid.main.search;
 
+import android.content.Context;
+import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
 
 import androidx.annotation.NonNull;
 import androidx.recyclerview.widget.RecyclerView;
 
+import com.example.meetingsystemandroid.R;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
+
 public class SearchActivityAdapter extends RecyclerView.Adapter<SearchActivityAdapter.SearchItemViewHolder> {
 
+    private Context mContext;
+
+    public SearchActivityAdapter(Context context) {
+        mContext = context;
+    }
     @NonNull
     @Override
     public SearchItemViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
-        return null;
+        View v = LayoutInflater.from(mContext).inflate(R.layout.recycler_search_result, parent, false);
+        return new SearchItemViewHolder(v);
     }
 
     @Override
     public void onBindViewHolder(@NonNull SearchItemViewHolder holder, int position) {
-
+        // 先占位看效果 之后变成加载搜索结果
+        holder.setActivityName("活动名称");
     }
 
     @Override
@@ -26,8 +41,16 @@ public class SearchActivityAdapter extends RecyclerView.Adapter<SearchActivityAd
 
     class SearchItemViewHolder extends RecyclerView.ViewHolder {
 
+        @BindView(R.id.search_activity_item_name)
+        TextView mActivityName;
+
         public SearchItemViewHolder(@NonNull View itemView) {
             super(itemView);
+            ButterKnife.bind(this, itemView);
+        }
+
+        public void setActivityName(String name) {
+            mActivityName.setText(name);
         }
     }
 }
--- app/src/main/java/com/example/meetingsystemandroid/main/search/SearchFragment.java
+++ app/src/main/java/com/example/meetingsystemandroid/main/search/SearchFragment.java
@@ -5,39 +5,33 @@ import android.net.Uri;
 import android.os.Bundle;
 
 import androidx.fragment.app.Fragment;
+import androidx.recyclerview.widget.DividerItemDecoration;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.LinearLayout;
+import android.widget.SearchView;
 
 import com.example.meetingsystemandroid.R;
 import com.example.meetingsystemandroid.main.home.HomePageFragment;
 
-/**
- * A simple {@link Fragment} subclass.
- * Use the {@link SearchFragment#newInstance} factory method to
- * create an instance of this fragment.
- */
+import butterknife.BindView;
+import butterknife.ButterKnife;
+import butterknife.Unbinder;
+
+
 public class SearchFragment extends Fragment {
 
-    public SearchFragment() {
-        // Required empty public constructor
-    }
+    private Unbinder unbinder;
+    @BindView(R.id.search_activity)
+    SearchView mSearchInput;
+    @BindView(R.id.search_activity_show)
+    RecyclerView mSearchResult;
 
-    /**
-     * Use this factory method to create a new instance of
-     * this fragment using the provided parameters.
-     *
-     * @param param1 Parameter 1.
-     * @param param2 Parameter 2.
-     * @return A new instance of fragment HomePageFragment.
-     */
-    // TODO: Rename and change types and number of parameters
-    public static HomePageFragment newInstance(String param1, String param2) {
-        HomePageFragment fragment = new HomePageFragment();
-        Bundle args = new Bundle();
-        fragment.setArguments(args);
-        return fragment;
+    public SearchFragment() {
     }
 
     @Override
@@ -48,12 +42,13 @@ public class SearchFragment extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_search, container, false);
-    }
-
-    // TODO: Rename method, update argument and hook method into UI event
-    public void onButtonPressed(Uri uri) {
+        View v = inflater.inflate(R.layout.fragment_search, container, false);
+        unbinder = ButterKnife.bind(this, v);
+        // init recyclerview
+        mSearchResult.setLayoutManager(new LinearLayoutManager(getContext()));
+        mSearchResult.setAdapter(new SearchActivityAdapter(getContext()));
+        mSearchResult.addItemDecoration(new DividerItemDecoration(getContext(), LinearLayout.VERTICAL));
+        return v;
     }
 
     @Override
@@ -65,4 +60,17 @@ public class SearchFragment extends Fragment {
     public void onDetach() {
         super.onDetach();
     }
+
+    @Override
+    public void onDestroyView() {
+        super.onDestroyView();
+        unbinder.unbind();
+    }
+
+    //    public static HomePageFragment newInstance(String param1, String param2) {
+//        HomePageFragment fragment = new HomePageFragment();
+//        Bundle args = new Bundle();
+//        fragment.setArguments(args);
+//        return fragment;
+//    }
 }
